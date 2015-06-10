539803e615077defe261ef9e7c59cba3c4e1e000
Evan Todd <et1337@gmail.com>
2015-06-10 10:54:56 -0400

    Never mind, CMake doesn't like missing source files.

---
+++ src/asset.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "types.h"
+
+struct Asset
+{
+	struct Model
+	{
+		static const size_t count = 3;
+		static const char* filenames[3];
+		static const AssetID Alpha = 1;
+		static const AssetID city3 = 2;
+	};
+	struct Texture
+	{
+		static const size_t count = 2;
+		static const char* filenames[2];
+		static const AssetID test = 1;
+	};
+	struct Shader
+	{
+		static const size_t count = 2;
+		static const char* filenames[2];
+		static const AssetID Standard = 1;
+	};
+};

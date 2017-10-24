9c13699847f860494b95b5c3db5dc3bbd4777755
Evan Todd <et1337@gmail.com>
2017-10-24 11:43:09 -0400

    Respect OS keyboard layout.

--- src/platform/sdl.cpp
+++ src/platform/sdl.cpp
@@ -430,6 +430,255 @@ namespace VI
 
 		SDL_PumpEvents();
 
+		KeyCode keymap[s32(KeyCode::count)];
+
+		{
+			SDL_Keycode default_keymap[SDL_NUM_SCANCODES] =
+			{
+				0, 0, 0, 0,
+				'a',
+				'b',
+				'c',
+				'd',
+				'e',
+				'f',
+				'g',
+				'h',
+				'i',
+				'j',
+				'k',
+				'l',
+				'm',
+				'n',
+				'o',
+				'p',
+				'q',
+				'r',
+				's',
+				't',
+				'u',
+				'v',
+				'w',
+				'x',
+				'y',
+				'z',
+				'1',
+				'2',
+				'3',
+				'4',
+				'5',
+				'6',
+				'7',
+				'8',
+				'9',
+				'0',
+				SDLK_RETURN,
+				SDLK_ESCAPE,
+				SDLK_BACKSPACE,
+				SDLK_TAB,
+				SDLK_SPACE,
+				'-',
+				'=',
+				'[',
+				']',
+				'\\',
+				'#',
+				';',
+				'\'',
+				'`',
+				',',
+				'.',
+				'/',
+				SDLK_CAPSLOCK,
+				SDLK_F1,
+				SDLK_F2,
+				SDLK_F3,
+				SDLK_F4,
+				SDLK_F5,
+				SDLK_F6,
+				SDLK_F7,
+				SDLK_F8,
+				SDLK_F9,
+				SDLK_F10,
+				SDLK_F11,
+				SDLK_F12,
+				SDLK_PRINTSCREEN,
+				SDLK_SCROLLLOCK,
+				SDLK_PAUSE,
+				SDLK_INSERT,
+				SDLK_HOME,
+				SDLK_PAGEUP,
+				SDLK_DELETE,
+				SDLK_END,
+				SDLK_PAGEDOWN,
+				SDLK_RIGHT,
+				SDLK_LEFT,
+				SDLK_DOWN,
+				SDLK_UP,
+				SDLK_NUMLOCKCLEAR,
+				SDLK_KP_DIVIDE,
+				SDLK_KP_MULTIPLY,
+				SDLK_KP_MINUS,
+				SDLK_KP_PLUS,
+				SDLK_KP_ENTER,
+				SDLK_KP_1,
+				SDLK_KP_2,
+				SDLK_KP_3,
+				SDLK_KP_4,
+				SDLK_KP_5,
+				SDLK_KP_6,
+				SDLK_KP_7,
+				SDLK_KP_8,
+				SDLK_KP_9,
+				SDLK_KP_0,
+				SDLK_KP_PERIOD,
+				0,
+				SDLK_APPLICATION,
+				SDLK_POWER,
+				SDLK_KP_EQUALS,
+				SDLK_F13,
+				SDLK_F14,
+				SDLK_F15,
+				SDLK_F16,
+				SDLK_F17,
+				SDLK_F18,
+				SDLK_F19,
+				SDLK_F20,
+				SDLK_F21,
+				SDLK_F22,
+				SDLK_F23,
+				SDLK_F24,
+				SDLK_EXECUTE,
+				SDLK_HELP,
+				SDLK_MENU,
+				SDLK_SELECT,
+				SDLK_STOP,
+				SDLK_AGAIN,
+				SDLK_UNDO,
+				SDLK_CUT,
+				SDLK_COPY,
+				SDLK_PASTE,
+				SDLK_FIND,
+				SDLK_MUTE,
+				SDLK_VOLUMEUP,
+				SDLK_VOLUMEDOWN,
+				0, 0, 0,
+				SDLK_KP_COMMA,
+				SDLK_KP_EQUALSAS400,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				SDLK_ALTERASE,
+				SDLK_SYSREQ,
+				SDLK_CANCEL,
+				SDLK_CLEAR,
+				SDLK_PRIOR,
+				SDLK_RETURN2,
+				SDLK_SEPARATOR,
+				SDLK_OUT,
+				SDLK_OPER,
+				SDLK_CLEARAGAIN,
+				SDLK_CRSEL,
+				SDLK_EXSEL,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				SDLK_KP_00,
+				SDLK_KP_000,
+				SDLK_THOUSANDSSEPARATOR,
+				SDLK_DECIMALSEPARATOR,
+				SDLK_CURRENCYUNIT,
+				SDLK_CURRENCYSUBUNIT,
+				SDLK_KP_LEFTPAREN,
+				SDLK_KP_RIGHTPAREN,
+				SDLK_KP_LEFTBRACE,
+				SDLK_KP_RIGHTBRACE,
+				SDLK_KP_TAB,
+				SDLK_KP_BACKSPACE,
+				SDLK_KP_A,
+				SDLK_KP_B,
+				SDLK_KP_C,
+				SDLK_KP_D,
+				SDLK_KP_E,
+				SDLK_KP_F,
+				SDLK_KP_XOR,
+				SDLK_KP_POWER,
+				SDLK_KP_PERCENT,
+				SDLK_KP_LESS,
+				SDLK_KP_GREATER,
+				SDLK_KP_AMPERSAND,
+				SDLK_KP_DBLAMPERSAND,
+				SDLK_KP_VERTICALBAR,
+				SDLK_KP_DBLVERTICALBAR,
+				SDLK_KP_COLON,
+				SDLK_KP_HASH,
+				SDLK_KP_SPACE,
+				SDLK_KP_AT,
+				SDLK_KP_EXCLAM,
+				SDLK_KP_MEMSTORE,
+				SDLK_KP_MEMRECALL,
+				SDLK_KP_MEMCLEAR,
+				SDLK_KP_MEMADD,
+				SDLK_KP_MEMSUBTRACT,
+				SDLK_KP_MEMMULTIPLY,
+				SDLK_KP_MEMDIVIDE,
+				SDLK_KP_PLUSMINUS,
+				SDLK_KP_CLEAR,
+				SDLK_KP_CLEARENTRY,
+				SDLK_KP_BINARY,
+				SDLK_KP_OCTAL,
+				SDLK_KP_DECIMAL,
+				SDLK_KP_HEXADECIMAL,
+				0, 0,
+				SDLK_LCTRL,
+				SDLK_LSHIFT,
+				SDLK_LALT,
+				SDLK_LGUI,
+				SDLK_RCTRL,
+				SDLK_RSHIFT,
+				SDLK_RALT,
+				SDLK_RGUI,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				SDLK_MODE,
+				SDLK_AUDIONEXT,
+				SDLK_AUDIOPREV,
+				SDLK_AUDIOSTOP,
+				SDLK_AUDIOPLAY,
+				SDLK_AUDIOMUTE,
+				SDLK_MEDIASELECT,
+				SDLK_WWW,
+				SDLK_MAIL,
+				SDLK_CALCULATOR,
+				SDLK_COMPUTER,
+				SDLK_AC_SEARCH,
+				SDLK_AC_HOME,
+				SDLK_AC_BACK,
+				SDLK_AC_FORWARD,
+				SDLK_AC_STOP,
+				SDLK_AC_REFRESH,
+				SDLK_AC_BOOKMARKS,
+				SDLK_BRIGHTNESSDOWN,
+				SDLK_BRIGHTNESSUP,
+				SDLK_DISPLAYSWITCH,
+				SDLK_KBDILLUMTOGGLE,
+				SDLK_KBDILLUMDOWN,
+				SDLK_KBDILLUMUP,
+				SDLK_EJECT,
+				SDLK_SLEEP,
+			};
+
+			for (s32 i = 0; i < s32(KeyCode::count); i++)
+			{
+				keymap[i] = KeyCode::None;
+
+				SDL_Keycode keycode = SDL_GetKeyFromScancode(SDL_Scancode(i));
+				for (s32 j = 0; j < s32(KeyCode::count); j++)
+				{
+					if (default_keymap[j] == keycode)
+					{
+						keymap[i] = KeyCode(j);
+						break;
+					}
+				}
+			}
+		}
+
 		const u8* sdl_keys = SDL_GetKeyboardState(0);
 
 		refresh_controllers();
@@ -490,6 +739,17 @@ namespace VI
 
 			SDL_PumpEvents();
 
+			sync->input.keys.clear();
+			for (s32 i = 0; i < s32(KeyCode::count); i++)
+			{
+				if (sdl_keys[i])
+				{
+					KeyCode code = keymap[i];
+					if (code != KeyCode::None)
+						sync->input.keys.set(s32(code), true);
+				}
+			}
+
 #if DEBUG
 			// screenshot
 			if (sync->input.keys.get(s32(KeyCode::F11)) && !sdl_keys[s32(KeyCode::F11)])
@@ -531,13 +791,6 @@ namespace VI
 				lodepng::encode("screen.png", data, w, h);
 			}
 #endif
-
-			sync->input.keys.clear();
-			for (s32 i = 0; i < s32(KeyCode::count); i++)
-			{
-				if (sdl_keys[i])
-					sync->input.keys.set(i, true);
-			}
 
 			SDL_Event sdl_event;
 			while (SDL_PollEvent(&sdl_event))

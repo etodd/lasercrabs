76c4da4c178320daaaed3b49181041fc2a77314f
Evan Todd <et1337@gmail.com>
2018-01-05 23:07:52 -0500

    Bump version number.

--- src/game/constants.h
+++ src/game/constants.h
@@ -11,7 +11,7 @@
 // if you change this, make sure to allocate more physics categories for each team's force field
 #define MAX_TEAMS 4
 
-#define GAME_VERSION 17
+#define GAME_VERSION 18
 
 #define STEAM_APP_ID 728100
 

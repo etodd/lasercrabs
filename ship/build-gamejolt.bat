fab758aa7e337fe7341e290fc37aa52d815f2a12
Evan Todd <et1337@gmail.com>
2018-02-23 17:04:04 -0500

    Add itch and gamejolt build scripts

---
+++ ship/build-gamejolt.bat
@@ -0,0 +1,4 @@
+cd "%~dp0"
+cp gamejolt-en/build.txt ../final/
+cp gamejolt-en/build.txt ../final-mac/
+cp gamejolt-en/build.txt ../final-linux/
---
+++ ship/itch.bat
@@ -0,0 +1,5 @@
+cd "%~dp0"
+cd ..
+butler push etodd/deceiver:win final
+butler push etodd/deceiver:linux final-linux
+butler push etodd/deceiver:mac final-mac

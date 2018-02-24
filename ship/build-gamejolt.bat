54e540d3492ca1441a85b66248e1fa1246b66c75
Evan Todd <et1337@gmail.com>
2018-02-24 14:11:03 -0500

    Update GameJolt build script

--- ship/build-gamejolt.bat
+++ ship/build-gamejolt.bat
@@ -1,4 +1,13 @@
 cd "%~dp0"
-cp gamejolt-en/build.txt ../final/
-cp gamejolt-en/build.txt ../final-mac/
-cp gamejolt-en/build.txt ../final-linux/
+rm deceiver-win.zip deceiver-mac.zip deceiver-linux.zip
+rm -rf final final-mac final-linux
+cp -r ../final ./final
+cp -r ../final-mac ./final-mac
+cp -r ../final-linux ./final-linux
+cp gamejolt-en/build.txt final
+cp gamejolt-en/build.txt final-mac/
+cp gamejolt-en/build.txt final-linux/
+powershell.exe -command "Compress-Archive -Path final/* -DestinationPath deceiver-win.zip"
+powershell.exe -command "Compress-Archive -Path final-mac/* -DestinationPath deceiver-mac.zip"
+powershell.exe -command "Compress-Archive -Path final-linux/* -DestinationPath deceiver-linux.zip"
+rm -rf final final-mac final-linux

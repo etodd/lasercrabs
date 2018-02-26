4e41f5b6456621aa798890f553c509cea2485658
Evan Todd <et1337@gmail.com>
2018-02-26 09:41:02 -0500

    Fix Gamejolt Mac build

--- ship/build-gamejolt.bat
+++ ship/build-gamejolt.bat
@@ -4,8 +4,8 @@ rm -rf final final-mac final-linux
 cp -r ../final ./final
 cp -r ../final-mac ./final-mac
 cp -r ../final-linux ./final-linux
-cp gamejolt-en/build.txt final
-cp gamejolt-en/build.txt final-mac/
+cp gamejolt-en/build.txt final/
+cp gamejolt-en/build.txt final-mac/DECEIVER.app/Contents/MacOS/
 cp gamejolt-en/build.txt final-linux/
 powershell.exe -command "Compress-Archive -Path final/* -DestinationPath deceiver-win.zip"
 powershell.exe -command "Compress-Archive -Path final-mac/* -DestinationPath deceiver-mac.zip"
